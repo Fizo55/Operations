50afd725bdddb2f1a478e15d1dfa6613e00e2c70
Fizo55 <hugomacees56000@gmail.com>
2021-09-24 18:52:42 +0200

    Add list, piles, files operations

---
+++ srcs/GenericOperations.py
@@ -0,0 +1,56 @@
+class GenericOperations:
+    def __init__(self, typeof = '', liste = ''):
+        self.liste = liste
+        self.typeof = typeof
+
+    def cree_objet_vide(self) -> bool:
+        self.liste = []
+        return self.liste
+
+    def objet_vide(self) -> bool:
+        return len(self.liste) == 0
+
+    def empiler(self, e) -> list:
+        if self.liste == '':
+            return self.cree_objet_vide()
+
+        if self.typeof == '':
+            self.liste.insert(0, e)
+        else:
+            self.liste.append(e)
+        return self.liste
+
+    def depiler(self) -> list:
+        if self.liste == '':
+            return self.cree_objet_vide()
+
+        if self.objet_vide():
+            return self.liste
+
+        elementSommet = self.liste[0]
+        del self.liste[0]
+        return elementSommet
+
+# Pile test
+interface = GenericOperations()
+interface.cree_objet_vide()
+interface.depiler()
+print(interface.objet_vide())
+interface.empiler("Bonjour")
+interface.empiler("Ceci")
+interface.empiler("Est")
+interface.empiler("Un")
+interface.empiler("Test")
+print(interface.depiler())
+
+# File test
+interface = GenericOperations('file')
+interface.cree_objet_vide()
+interface.depiler()
+print(interface.objet_vide())
+interface.empiler("Bonjour")
+interface.empiler("Ceci")
+interface.empiler("Est")
+interface.empiler("Un")
+interface.empiler("Test")
+print(interface.depiler())
---
+++ srcs/ListOperations.py
@@ -0,0 +1,107 @@
+class ListOperationsInterface:
+    def __init__(self, liste = ''):
+        self.liste = liste
+
+    def creer_liste_vide(self) -> list:
+        """
+            Entrée: None
+            Sortie: List
+            Créer une liste vide et retourne vrai si la liste retourné est nulle sinon retourne faux
+        """
+
+        self.liste = []
+        return self.liste == []
+
+    def inserer(self, e, i) -> list:
+        """
+            Entrées: Element e(str), Index i(int)
+            Sortie: List
+            Retourne la liste avec l'élement insérer
+        """
+
+        if self.longueur() < 0: # Vous devriez utiliser la fonction "creer_liste_vide" dans votre programme principal
+            return False
+
+        self.liste.insert(i, e)
+        return self.liste
+
+    def rechercher(self, e) -> int:
+        """
+            Entrée: Element e(str)
+            Sortie: -1 si l'élement n'est pas dans la liste sinon l'index de l'élement
+            Cherche un élement dans la liste et retourne -1 si il n'est pas trouvé sinon l'élement de l'index est retourné.
+        """
+
+        if e not in self.liste:
+            return -1
+
+        return self.liste.index(e)
+
+    def lire(self, i) -> object:
+        """
+            Entrée: Index i(int)
+            Sortie: False si la liste n'existe pas sinon l'élement de la liste est retourné
+            Récupère un élement de la liste et le retourne (si la liste n'existe pas retourne faux)
+        """
+
+        if self.longueur() < 0:
+            return False
+
+        return self.liste[i]
+
+    def supprimer(self, i) -> list:
+        """
+            Entrée: Index i(int)
+            Sortie: False si la liste n'existe pas sinon l'élement de la liste est supprimé
+            Supprime un élement de la liste et retourne la liste modifiée (si la liste n'existe pas retourne faux)
+        """
+
+        if self.longueur() < i:
+            return False
+
+        del self.liste[i]
+        return self.liste
+
+    def longueur(self) -> int:
+        """
+            Entrée: None
+            Sortie: len(liste)
+            Retourne la longueur de la liste
+        """
+
+        return len(self.liste)
+
+    def est_liste_vide(self) -> bool:
+        """
+            Entrée: None
+            Sortie: vrai si la liste est vide faux sinon
+        """
+
+        return self.liste == []
+
+    def est_dans(self, e) -> bool:
+        return True if e in self.liste else False
+
+    def modifier(self, e, i) -> list:
+        if self.longueur() - 1 < i:
+            return self.liste
+        self.liste[i] = e
+        return self.liste
+
+interface = ListOperationsInterface()
+interface.creer_liste_vide()
+
+interface.inserer("Bonjour", 0)
+interface.inserer("Ceci", 1)
+interface.inserer("Est", 2)
+interface.inserer("Un", 3)
+interface.inserer("Test", 4)
+
+texte = interface.lire(0)
+print(texte)
+
+longueur = interface.longueur()
+print(longueur)
+
+modification = interface.modifier("Bonsoir", 0)
+print(modification)
